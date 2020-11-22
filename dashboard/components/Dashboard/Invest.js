b20004867dc50ec7f0df57d345f5a5851b477029
rachit2501 <rac.sri25@gmail.com>
2020-11-22 17:30:25 +0530

    inverst working

--- dashboard/components/Dashboard/Invest.js
+++ dashboard/components/Dashboard/Invest.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import { TextField, Button, Container } from '@material-ui/core';
+import { depositToThePool } from '../../utils/ethereum/utils/user.ts';
+import { depositToThePool as depositConflux } from '../../utils/conflux/user.ts';
+import loadBlockchain from '../../utils/ethereum/loadBlockchainData.ts';
 
 const Invest = () => {
 	const [eth, updateETH] = useState(0);
-	const [one, updateOne] = useState(0);
+	const [CFX, updateCFX] = useState(0);
 
-	const onSubmitOne = async () => {
-		console.log(eth, one);
+	const onSubmitCFX = async () => {
+		console.log(eth, CFX);
+		const reciept = await depositConflux(CFX);
+		console.log(reciept);
 	};
 
 	const onSubmitETH = async () => {
-		console.log(eth, one);
+		console.log(eth, CFX);
+		const { Contract, accounts } = await loadBlockchain();
+		depositToThePool(Contract, accounts, eth);
 	};
 
 	return (
@@ -45,13 +52,13 @@ const Invest = () => {
 						label="Deposit CRX"
 						variant="outlined"
 						margin="dense"
-						onChange={(e) => updateOne(e.target.value)}
+						onChange={(e) => updateCFX(e.target.value)}
 					/>
 					<br />
 					<Button
 						variant="contained"
 						color="primary"
-						onClick={() => onSubmitOne()}
+						onClick={() => onSubmitCFX()}
 					>
 						Deposit
 					</Button>
